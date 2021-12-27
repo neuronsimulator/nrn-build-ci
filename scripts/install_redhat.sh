245ec73dd8759f990a93a80162e85067d0ac27ba
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2021-12-27 12:08:01 +0100

    temporarily test PR branch instead of installing openssl

--- scripts/install_redhat.sh
+++ scripts/install_redhat.sh
@@ -4,5 +4,4 @@ CMD=$(command -v dnf || command -v yum)
 ${CMD} update -y
 ${CMD} install -y bison cmake diffutils dnf flex gcc gcc-c++ git \
   openmpi-devel libXcomposite-devel libXext-devel make readline-devel ncurses-devel \
-  ninja-build python3-devel python3-pip python3-wheel sudo which wget unzip \
-  openssl-devel  # for tqperf integration test
+  ninja-build python3-devel python3-pip python3-wheel sudo which wget unzip
