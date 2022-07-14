9854a7764792318a897972440f9d31d8ee657a26
Olli Lupton <oliver.lupton@epfl.ch>
2022-07-14 10:38:56 +0200

    debian:buster: skip boost

--- scripts/install_debian_debian_buster.sh
+++ scripts/install_debian_debian_buster.sh
@@ -2,6 +2,4 @@
 echo 'deb http://deb.debian.org/debian buster-backports main' >> /etc/apt/sources.list
 apt-get update
 apt-get install -t buster-backports -y cmake
-# Set MPICH to be the default MPI
-apt-get install -y mpich
-update-alternatives --set mpi /usr/bin/mpicc.mpich
+DO_NOT_INSTALL_BOOST=true
