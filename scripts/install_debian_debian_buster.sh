e225c6979c9083f2bd7df06c2587091493367fc7
Olli Lupton <oliver.lupton@epfl.ch>
2022-07-14 10:07:39 +0200

    debian:buster: try and prefer MPICH.

--- scripts/install_debian_debian_buster.sh
+++ scripts/install_debian_debian_buster.sh
@@ -2,3 +2,6 @@
 echo 'deb http://deb.debian.org/debian buster-backports main' >> /etc/apt/sources.list
 apt-get update
 apt-get install -t buster-backports -y cmake
+# Set MPICH to be the default MPI
+apt-get install mpich
+update-alternatives --set mpi /usr/bin/mpicc.mpich
