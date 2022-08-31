97501d2130844e4ba66d4618224e4007f12d40ca
Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
2022-08-31 16:16:58 +0200

    take fedora into account

--- scripts/environment_redhat.sh
+++ scripts/environment_redhat.sh
@@ -12,18 +12,21 @@ source /etc/profile.d/modules.sh
 # implementation/version of MPI.
 module load mpi
 
-# Get the CENTOS VERSION for specific enviroment setup
-CENTOS_VERSION=$(rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release))
-echo "CENTOS_VERSION: ${CENTOS_VERSION}"
+# if on CENTOS
+if [[ "$(rpm -E %centos)" != "%centos" ]]; then
 
-# CENTOS 7
-if [[ $CENTOS_VERSION == 7* ]]; then
-    # Ensure that the newer CMake version that was installed on CentOS7 is
-    # preferred over the original system version.
-    export PATH=/usr/local/sbin:/usr/local/bin:${PATH}
-# CENTOS 8    
-elif [[ $CENTOS_VERSION == 8* ]]; then
-    # Enable GCC 9
-    source /opt/rh/gcc-toolset-9/enable
+    # Get the CENTOS VERSION for specific enviroment setup
+    CENTOS_VERSION=$(rpm -E %centos)
+    echo "CENTOS_VERSION: ${CENTOS_VERSION}"
+
+    # CENTOS 7
+    if [[ $CENTOS_VERSION == 7 ]]; then
+        # Ensure that the newer CMake version that was installed on CentOS7 is
+        # preferred over the original system version.
+        export PATH=/usr/local/sbin:/usr/local/bin:${PATH}
+    # CENTOS 8    
+    elif [[ $CENTOS_VERSION == 8 ]]; then
+        # Enable GCC 9
+        source /opt/rh/gcc-toolset-9/enable
+    fi
 fi
-
