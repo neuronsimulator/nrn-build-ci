4e5547c7fdc7c44369c55ac57e0e3f4e8ff0115e
Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
2022-08-31 15:43:39 +0200

    centos8: use GCC 9 for c++17 support

--- scripts/environment_redhat.sh
+++ scripts/environment_redhat.sh
@@ -11,3 +11,19 @@ source /etc/profile.d/modules.sh
 # Assume we only installed one version; otherwise you would have to specify an
 # implementation/version of MPI.
 module load mpi
+
+# Get the CENTOS VERSION for specific enviroment setup
+CENTOS_VERSION=$(rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release))
+echo "CENTOS_VERSION: ${CENTOS_VERSION}"
+
+# CENTOS 7
+if [[ $CENTOS_VERSION == 7* ]]; then
+    # Ensure that the newer CMake version that was installed on CentOS7 is
+    # preferred over the original system version.
+    export PATH=/usr/local/sbin:/usr/local/bin:${PATH}
+# CENTOS 8    
+elif [[ $CENTOS_VERSION == 8* ]]; then
+    # Enable GCC 9
+    source /opt/rh/gcc-toolset-9/enable
+fi
+
