9e9f816ad0d2588f95cab4ab0b1f21a33ba21515
Pramod S Kumbhar <pramod.s.kumbhar@gmail.com>
2022-09-06 12:26:56 +0200

    revert unnecessary changes and some cleanup

--- scripts/install_redhat.sh
+++ scripts/install_redhat.sh
@@ -4,12 +4,7 @@ CMD=$(command -v dnf || command -v yum)
 
 # Use mpich on centos7 otherwise use openmpi
 # See neuronsimulator/nrn-build-ci/pull/51
-if cat /etc/*release | grep -q CentOS-7;
-then
-    mpi_lib=mpich-devel
-else
-    mpi_lib=openmpi-devel
-fi
+mpi_lib=$(if cat /etc/*release | grep -q CentOS-7; then echo "mpich-devel"; else echo "openmpi-devel"; fi)
 
 ${CMD} update -y
 ${CMD} install -y bison boost-devel cmake diffutils dnf findutils \
