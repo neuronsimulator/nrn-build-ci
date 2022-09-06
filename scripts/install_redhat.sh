eea231a3bb7c205e9f6c869ad7f75394b7cb9f66
Pramod S Kumbhar <pramod.s.kumbhar@gmail.com>
2022-09-06 14:19:00 +0200

    address review comments

--- scripts/install_redhat.sh
+++ scripts/install_redhat.sh
@@ -2,10 +2,6 @@
 # Use DNF if available (not CentOS7), otherwise YUM
 CMD=$(command -v dnf || command -v yum)
 
-# Use mpich on centos7 otherwise use openmpi
-# See neuronsimulator/nrn-build-ci/pull/51
-mpi_lib=$(if cat /etc/*release | grep -q CentOS-7; then echo "mpich-devel"; else echo "openmpi-devel"; fi)
-
 ${CMD} update -y
 ${CMD} install -y bison boost-devel cmake diffutils dnf findutils \
   flex gcc gcc-c++ git ${mpi_lib:-openmpi-devel} libXcomposite-devel \
