c9552872e0b548ce49e794ec53c9e5741114f398
Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
2022-09-13 15:46:48 +0200

    fedora35: rely on procps (#54)
    * have a working `/usr/bin/ps`
    * used to determine `my_shell` in `/etc/profile.d/modules.sh`

--- scripts/install_redhat.sh
+++ scripts/install_redhat.sh
@@ -3,7 +3,7 @@
 CMD=$(command -v dnf || command -v yum)
 
 ${CMD} update -y
-${CMD} install -y bison boost-devel cmake diffutils dnf findutils \
+${CMD} install -y procps bison boost-devel cmake diffutils dnf findutils \
   flex gcc gcc-c++ git ${mpi_lib:-openmpi-devel} libXcomposite-devel \
   libXext-devel make openssl-devel python3-devel readline-devel \
   ncurses-devel ninja-build sudo which wget unzip
