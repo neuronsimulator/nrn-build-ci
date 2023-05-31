e288761a8c85a0658e9648719f6d66b346f120cd
Olli Lupton <oliver.lupton@epfl.ch>
2023-05-31 11:00:43 +0200

    CoreNEURON => NMODL => sympy + new bison (#60)

--- scripts/environment_macOS.sh
+++ scripts/environment_macOS.sh
@@ -3,5 +3,7 @@
 export SKIP_EMBEDED_PYTHON_TEST=true
 # Do not enable OpenMP on macOS
 export CORENRN_ENABLE_OPENMP=OFF
+# Use Bison from homebrew
+export PATH=/usr/local/opt/bison/bin:${PATH}
 # Use Flex from homebrew
 export PATH=/usr/local/opt/flex/bin:${PATH}
