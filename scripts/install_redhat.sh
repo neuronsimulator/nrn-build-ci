20d5e334b1be64a09bc056feeefb94fef2487f93
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-01-22 11:21:25 +0100

    Setup debugging

--- scripts/install_redhat.sh
+++ scripts/install_redhat.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+set -eux
 # Use DNF if available (not CentOS7), otherwise YUM
 CMD=$(command -v dnf || command -v yum)
 
@@ -7,3 +9,5 @@ ${CMD} install -y procps bison boost-devel cmake diffutils dnf findutils \
   flex gcc gcc-c++ git ${mpi_lib:-openmpi-devel} libXcomposite-devel \
   libXext-devel make openssl-devel python3.11-devel readline-devel \
   ncurses-devel ninja-build sudo which wget unzip
+
+set +eux
