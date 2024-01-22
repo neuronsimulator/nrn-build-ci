b3d7248542cb5031b4cf0b3c2a7c2d85e049a171
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-01-22 10:57:30 +0100

    Fix Fedora latest Python version

--- scripts/install_redhat.sh
+++ scripts/install_redhat.sh
@@ -2,8 +2,17 @@
 # Use DNF if available (not CentOS7), otherwise YUM
 CMD=$(command -v dnf || command -v yum)
 
+if [[ "${OS_CONTAINER}" == 'fedora:latest' ]]
+then
+    # on Fedora latest we explicitly pin 3.11 for now
+    PYTHON_VERSION="3.11"
+else
+    # use the default one
+    PYTHON_VERSION="3"
+fi
+
 ${CMD} update -y
 ${CMD} install -y procps bison boost-devel cmake diffutils dnf findutils \
   flex gcc gcc-c++ git ${mpi_lib:-openmpi-devel} libXcomposite-devel \
-  libXext-devel make openssl-devel python3-devel readline-devel \
+  libXext-devel make openssl-devel python"${PYTHON_VERSION}"-devel readline-devel \
   ncurses-devel ninja-build sudo which wget unzip
