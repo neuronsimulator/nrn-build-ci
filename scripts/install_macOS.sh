38714a84157aefca6d85e99ced3164aa4513da04
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-03-10 12:46:04 +0100

    Fix for MacOS 13
    When running the CI, we get errors like:
    
    ==> Pouring python@3.11--3.11.8.ventura.bottle.tar.gz
    Error: The `brew link` step did not complete successfully
    The formula built, but is not symlinked into /usr/local
    Could not symlink bin/2to3-3.11
    Target /usr/local/bin/2to3-3.11
    already exists. You may want to remove it:
      rm '/usr/local/bin/2to3-3.11'
    
    To force the link and overwrite all conflicting files:
      brew link --overwrite python@3.11
    
    To list all files that would be deleted:
      brew link --overwrite python@3.11 --dry-run
    
    Possible conflicting files are:
    /usr/local/bin/2to3-3.11 ->
    /Library/Frameworks/Python.framework/Versions/3.11/bin/2to3-3.11
    /usr/local/bin/idle3.11 ->
    /Library/Frameworks/Python.framework/Versions/3.11/bin/idle3.11
    /usr/local/bin/pydoc3.11 ->
    /Library/Frameworks/Python.framework/Versions/3.11/bin/pydoc3.11
    /usr/local/bin/python3.11 ->
    /Library/Frameworks/Python.framework/Versions/3.11/bin/python3.11
    /usr/local/bin/python3.11-config ->
    /Library/Frameworks/Python.framework/Versions/3.11/bin/python3.11-config
    
    Hence, we use a fix described here:
    https://github.com/actions/setup-python/issues/577#issuecomment-1365231818

--- scripts/install_macOS.sh
+++ scripts/install_macOS.sh
@@ -1,3 +1,10 @@
+set -eux
+# Temporary fix, see https://github.com/actions/setup-python/issues/577
+rm /usr/local/bin/2to3 || true
+rm /usr/local/bin/idle3 || true
+rm /usr/local/bin/pydoc3 || true
+rm /usr/local/bin/python3 || true
+rm /usr/local/bin/python3-config || true
 brew install bison boost coreutils flex mpich ninja xz wget
 brew unlink mpich
 brew install openmpi
