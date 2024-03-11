ebd39d8ea7bb27cec2123aa326f03a259eebd6b7
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-03-11 08:37:57 +0100

    Maybe say "set it to 1" instead of "set it", how about that?

--- scripts/install_macOS.sh
+++ scripts/install_macOS.sh
@@ -1,6 +1,6 @@
 set -eux
-export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=''
-export HOMEBREW_NO_INSTALL_UPGRADE=''
+export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
+export HOMEBREW_NO_INSTALL_UPGRADE=1
 brew install bison boost coreutils flex mpich ninja xz wget
 brew unlink mpich
 brew install openmpi
