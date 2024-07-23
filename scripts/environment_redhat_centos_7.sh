64e4139d66e6c47ff1a1aef174684afebd8f9a41
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-07-23 11:08:34 +0200

    Remove CentOS 7

--- scripts/environment_redhat_centos_7.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Ensure that the newer CMake version that was installed on CentOS7 is
-# preferred over the original system version.
-export PATH=/usr/local/sbin:/usr/local/bin:${PATH}
