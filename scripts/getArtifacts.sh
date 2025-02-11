f623988e9b274222566a13b4f8c2d280bc4353dd
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2025-02-11 17:05:18 +0100

    Use wget instead of curl

--- scripts/getArtifacts.sh
+++ scripts/getArtifacts.sh
@@ -17,11 +17,11 @@ if [[ "${ARTIFACT_DROP_URL}" =~ https://github.com/neuronsimulator.* ]]
 then
     ARTIFACT_ID=$(echo "${ARTIFACT_DROP_URL}" | grep --color=never -oE '[0-9]+$')
     echo "Extracted Artifact ID: ${ARTIFACT_ID}"
-    curl -L -sfS \
-      -H "Accept: application/vnd.github+json" \
-      -H "Authorization: Bearer ${NRN_MODELDB_CI_TOKEN}" \
-      -H "X-GitHub-Api-Version: 2022-11-28" \
-      "https://api.github.com/repos/neuronsimulator/nrn/actions/artifacts/${ARTIFACT_ID}/zip" -o drop.zip
+    wget --header="Accept: application/vnd.github+json" \
+         --header="Authorization: Bearer ${NRN_MODELDB_CI_TOKEN}" \
+         --header="X-GitHub-Api-Version: 2022-11-28" \
+         --quiet --show-progress --output-document=drop.zip \
+         "https://api.github.com/repos/neuronsimulator/nrn/actions/artifacts/${ARTIFACT_ID}/zip"
     unzip -d drop drop.zip
 else
     wget --tries=4 -LO drop.zip "${ARTIFACT_DROP_URL}"
