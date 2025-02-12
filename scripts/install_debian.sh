3d192eb03f2c18f7561a5cae9ec25c58eed20db6
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2025-02-12 14:50:21 +0100

    Use env to set min Python version

--- scripts/install_debian.sh
+++ scripts/install_debian.sh
@@ -8,13 +8,15 @@ export DEBIAN_FRONTEND=noninteractive
 # --- End GitHub-Actions-specific code ---
 # ---
 apt-get update
-apt-get install -y bison cmake flex git libncurses-dev libmpich-dev libssl-dev \
+apt-get install -y software-properties-common bison cmake flex git libncurses-dev libmpich-dev libssl-dev \
   libx11-dev libxcomposite-dev ninja-build mpich libreadline-dev sudo wget unzip curl
 if [[ "${DO_NOT_INSTALL_BOOST}" != "true" ]]; then
   apt-get install -y libboost-all-dev
 fi
 if [[ -z "${NRN_PYTHON}" ]]; then
-  apt-get install -y python3-dev python3-venv
-  export NRN_PYTHON=$(command -v python3)
+  add-apt-repository -y ppa:deadsnakes/ppa
+  apt-get install -y "python${MIN_PYTHON_MAJOR_VERSION}.${MIN_PYTHON_MINOR_VERSION}-dev" "python${MIN_PYTHON_MAJOR_VERSION}.${MIN_PYTHON_MINOR_VERSION}-venv"
+  NRN_PYTHON="$(command -v "python${MIN_PYTHON_MAJOR_VERSION}.${MIN_PYTHON_MINOR_VERSION}")"
+  export NRN_PYTHON
   echo "NRN_PYTHON=${NRN_PYTHON}" >> $GITHUB_ENV
 fi
--- scripts/install_redhat_almalinux_8_10.sh
+++ scripts/install_redhat_almalinux_8_10.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Enable the (official) PowerTools repository. This provides Ninja.
-dnf install -y dnf-plugins-core python39-devel gcc-toolset-9-gcc gcc-toolset-9-gcc-c++
-export NRN_PYTHON=$(command -v python3.9)
+dnf install -y dnf-plugins-core "python${MIN_PYTHON_MAJOR_VERSION}${MIN_PYTHON_MINOR_VERSION}-devel" gcc-toolset-9-gcc gcc-toolset-9-gcc-c++
+NRN_PYTHON="$(command -v "python${MIN_PYTHON_MAJOR_VERSION}.${MIN_PYTHON_MINOR_VERSION}")"
+export NRN_PYTHON
 echo "NRN_PYTHON=${NRN_PYTHON}" >> $GITHUB_ENV
 dnf config-manager --set-enabled powertools
